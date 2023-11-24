c8a152c84fe16910ebab40cc64c4a7b88070c324
lorenzo270704 <denslobe123@gmail.com>
2023-11-23 21:20:36 -0300

    Projeto Multimarcas

---
+++ src/lojaderoupa/dao/conexao.java
@@ -0,0 +1,15 @@
+package lojaderoupa.dao;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 182200155
+ */
+public class conexao {
+    
+}
