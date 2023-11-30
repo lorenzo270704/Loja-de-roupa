b67f474f585c0649f7d6d1b1af43319a1cb41428
lorenzo270704 <denslobe123@gmail.com>
2023-11-30 20:55:37 -0300

    CriacaoDeClasses

--- src/lojaderoupa/controller/CliController.java
+++ src/lojaderoupa/controller/CliController.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package lojaderoupa.view;
+package lojaderoupa.controller;
 
 /**
  *
  * @author 182200155
  */
-public class Teste {
+public class CliController {
     
 }
---
+++ src/lojaderoupa/model/ProdutoPedido.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lojaderoupa.model;
+
+/**
+ *
+ * @author 182200155
+ */
+public class ProdutoPedido {
+    
+}
