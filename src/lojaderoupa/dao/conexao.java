df3ec8ff541017e5e66e8ca2c55d1c1cf38aa710
lorenzo270704 <denslobe123@gmail.com>
2023-11-29 21:41:04 -0300

    BancoDDados

--- src/lojaderoupa/dao/conexao.java
+++ src/lojaderoupa/dao/conexao.java
@@ -1,5 +1,11 @@
 package lojaderoupa.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,5 +17,38 @@ package lojaderoupa.dao;
  * @author 182200155
  */
 public class conexao {
+     
+    private static final String URL = "jdbc:mysql://localhost:3306/loja";
+    private static final String USER = "root";
     
+    
+    
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String SENHA = "";
+    
+    public static void executar(String query){
+        try {
+            Class.forName( DRIVER );
+            Connection conn = DriverManager.getConnection(URL,USER,SENHA);
+            Statement st = conn.createStatement();
+            st.execute( query );
+            conn.close();
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.toString());
+        }
+    }
+    
+    public static ResultSet consultar(String query){
+        try {
+            Class.forName( DRIVER );
+            Connection conn = DriverManager.getConnection(URL,USER,SENHA);
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery( query );
+            return rs;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.toString());
+            return null;
+        }
+    }   
 }
