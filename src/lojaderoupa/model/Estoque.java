ce1b9996e15f137a480de38ac2d1fcc79df62672
ChayRR12 <chayelek@outlook.com>
2023-12-08 21:24:38 -0300

    Construtor estoque

--- src/lojaderoupa/model/Estoque.java
+++ src/lojaderoupa/model/Estoque.java
@@ -13,5 +13,36 @@ public class Estoque {
     private int idEstoque;
     private String quantidadeEst;
     private Produto produto;
+
+    public int getIdEstoque() {
+        return idEstoque;
+    }
+
+    public void setIdEstoque(int idEstoque) {
+        this.idEstoque = idEstoque;
+    }
+
+    public String getQuantidadeEst() {
+        return quantidadeEst;
+    }
+
+    public void setQuantidadeEst(String quantidadeEst) {
+        this.quantidadeEst = quantidadeEst;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Estoque(int idEstoque, String quantidadeEst, Produto produto) {
+        this.idEstoque = idEstoque;
+        this.quantidadeEst = quantidadeEst;
+        this.produto = produto;
+    }
+    
     
 }
